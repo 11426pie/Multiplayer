1d9c39868003e39409048762f8a3af4ea303b8dd
Subin <subjsam@gmail.com>
2015-12-23 22:47:10 -0500

    initial commit

---
+++ desktop/src/com/mygdx/multiplayer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.multiplayer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.multiplayer.MultiPlayer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MultiPlayer(), config);
+	}
+}
